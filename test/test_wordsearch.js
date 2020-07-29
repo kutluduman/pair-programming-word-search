9188ca92a6b3c5c00e87202ac71eed82c1a612e1
Kutluhan Duman <kutluduman@gmail.com>
2020-07-29 22:21:51 +0000

    Implemented the function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -23,10 +23,10 @@ describe("#wordSearch()", function() {
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
@@ -35,4 +35,22 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+    assert.isTrue(result);
+  });
+
+
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,62 @@
-const wordSearch = (letters, word) => { 
+// pair programming with Michele B. on July 29th
+const wordSearch = (letters, word) => {
+    // if matrix is empty, or it is undefined, returns undefined
+    if (letters.length === 0) {
+        return undefined;
+    }
+
+    // this function will iterate through the array and check whether the array contains the word horizontally. If yes, it returns true
+    function checkHorizontal(matrix, target) {
+        for (let i = 0; i < matrix[0].length; i++) {
+            let result = '';
+            for (const el of matrix) {
+                result += el[i];
+                if (result.includes(word)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    // checks if the function above, ran with matrix and word as arguments, is true. if so, main function returns true
+    if (checkHorizontal(letters, word) === true) {
+        return true;
+    }
+    // if above if ^ is false, then matrix is transposed (using function from yesterday pp exercise).
+    let transposed = transpose(letters);
+
+    // we reuse our function, now against the transposed matrix. same if statement as before
+    if (checkHorizontal(transposed) === true) {
+        return true;
+    // if word cannot be found in either original matrix nor transposed, returns false
+    } else {
+        return false;
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+        if (l.includes(word)) {
+            return true;
+        }
 
-module.exports = wordSearch
+    }
+    return false;
+};
+
+const transpose = function (matrix) {
+    let output = [];
+    const height = matrix.length;
+    const width = matrix[0].length;
+    for (let j = 0; j < width; j++) {
+        let array = [];
+        for (let i = 0; i < height; i++) {
+            array.push(matrix[i][j])
+        }
+        output.push(array);
+    }
+    return output;
+};
+
+
+
+module.exports = wordSearch;
